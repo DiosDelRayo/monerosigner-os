cb0fafb5571c6bc733f8f25f9a07e97b617ab2c1
who asks? <user@localhost>
2024-06-27 14:46:10 -0600

    startup

--- opt/rootfs-overlay/start.sh
+++ opt/rootfs-overlay/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+# TODO: 2024-07-15, can be removed the day XmrSigner will be installed as buildroot package
 cd /opt/src/
 
 #/usr/bin/python3 main.py >> /dev/kmsg 2>&1 &  # version that writes output to dmesg
-/usr/bin/python3 main.py &
+/usr/bin/python3 -m xmrsigner &
