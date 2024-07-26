c615644183e32ae99a8fb5dc7f2a267f9abd8944
who asks? <user@localhost>
2024-07-26 12:00:37 -0600

    reached a dead end, or going even more the rabbit hole down to build dependencies not even used => absolute insanity!!!!

--- opt/external-packages/monero-wallet-rpc-xmrsigner/monero-wallet-rpc-xmrsigner.mk
+++ opt/external-packages/monero-wallet-rpc-xmrsigner/monero-wallet-rpc-xmrsigner.mk
@@ -9,14 +9,23 @@ MONERO_WALLET_RPC_XMRSIGNER_SOURCE = monero-wallet-rpc_linux_armv6_static.tar.bz
 MONERO_WALLET_RPC_XMRSIGNER_SITE = https://github.com/DiosDelRayo/monero/releases/download/v$(MONERO_WALLET_RPC_XMRSIGNER_VERSION)
 MONERO_WALLET_RPC_XMRSIGNER_LICENSE = BSD-3-Clause
 MONERO_WALLET_RPC_XMRSIGNER_LICENSE_FILES = LICENSE
+MONERO_WALLET_RPC_XMRSIGNER_DEPENDENCIES = \
+    openssl \
+    boost \
+    libunwind \
+    unbound \
+    hidapi \
+    libusb \
+    protobuf \
+    zeromq
 
 define MONERO_WALLET_RPC_XMRSIGNER_EXTRACT_CMDS
-	mkdir -p $(@D)
-	tar -xjf /buildroot_dl/monero-wallet-rpc-xmrsigner/$(MONERO_WALLET_RPC_XMRSIGNER_SOURCE) -C $(@D)
+    mkdir -p $(@D)
+    $(TAR) -xjf $(MONERO_WALLET_RPC_XMRSIGNER_DL_DIR)/$(MONERO_WALLET_RPC_XMRSIGNER_SOURCE) -C $(@D)
 endef
 
 define MONERO_WALLET_RPC_XMRSIGNER_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/monero-wallet-rpc $(TARGET_DIR)/usr/bin/monero-wallet-rpc
+    $(INSTALL) -D -m 0755 $(@D)/monero-wallet-rpc $(TARGET_DIR)/usr/bin/monero-wallet-rpc
 endef
 
-$(eval $(generic-package))
+$(eval $(binary-package))
