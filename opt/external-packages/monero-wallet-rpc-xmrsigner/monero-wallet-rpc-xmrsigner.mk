3e0ec21b81789e45d6a9755e8bf2044911fed7ca
who asks? <user@localhost>
2024-08-22 13:32:17 -0600

    fix monero-wallet-rpc, add stack smach protection

--- opt/external-packages/monero-wallet-rpc-xmrsigner/monero-wallet-rpc-xmrsigner.mk
+++ opt/external-packages/monero-wallet-rpc-xmrsigner/monero-wallet-rpc-xmrsigner.mk
@@ -4,20 +4,11 @@
 #
 ################################################################################
 
-MONERO_WALLET_RPC_XMRSIGNER_VERSION = 0.18.3.3-xmrsigner
+MONERO_WALLET_RPC_XMRSIGNER_VERSION = 0.18.3.4-xmrsigner
 MONERO_WALLET_RPC_XMRSIGNER_SOURCE = monero-wallet-rpc_linux_armv6_static.tar.bz2
 MONERO_WALLET_RPC_XMRSIGNER_SITE = https://github.com/DiosDelRayo/monero/releases/download/v$(MONERO_WALLET_RPC_XMRSIGNER_VERSION)
 MONERO_WALLET_RPC_XMRSIGNER_LICENSE = BSD-3-Clause
 MONERO_WALLET_RPC_XMRSIGNER_LICENSE_FILES = LICENSE
-MONERO_WALLET_RPC_XMRSIGNER_DEPENDENCIES = \
-    openssl \
-    boost \
-    libunwind \
-    unbound \
-    hidapi \
-    libusb \
-    protobuf \
-    zeromq
 
 define MONERO_WALLET_RPC_XMRSIGNER_EXTRACT_CMDS
     mkdir -p $(@D)
@@ -28,4 +19,4 @@ define MONERO_WALLET_RPC_XMRSIGNER_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/monero-wallet-rpc $(TARGET_DIR)/usr/bin/monero-wallet-rpc
 endef
 
-$(eval $(binary-package))
+$(eval $(generic-package))
