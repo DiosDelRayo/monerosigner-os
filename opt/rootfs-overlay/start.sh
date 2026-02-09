497f227c5006562c16b56c679bc19775597a6448
who asks? <user@localhost>
2026-02-09 04:01:25 -0600

    save for now

--- opt/rootfs-overlay/start.sh
+++ opt/rootfs-overlay/start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# TODO: 2026-02-09, can't remember why added, but pretty sure has to do with the attempt to ssh into the dev build, so probably to remove later
+ulimit -l $(ulimit -Hl)
+
 ip link set lo up
 
 # TODO: 2024-07-15, can be removed the day XmrSigner will be installed as buildroot package
